1c6654b7c69266a37fe9687ffb628b1c9815f586
capacitytools <capacitydotbuild@gmail.com>
2026-09-15 09:47:32 +0100

    Update PWAregister.js

--- components/PWAregister.js
+++ components/PWAregister.js
@@ -8,12 +8,10 @@ export default function PWARegister() {
     navigator.serviceWorker
       .register('/sw.js')
       .then((reg) => {
-        // Check for SW updates every 60 minutes
         setInterval(() => reg.update(), 60 * 60 * 1000);
       })
       .catch(() => {});
 
-    // Reload once when new SW takes over
     let refreshed = false;
     navigator.serviceWorker.addEventListener('controllerchange', () => {
       if (refreshed) return;
